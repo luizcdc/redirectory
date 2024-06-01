55d6567904c9754d76860c3536defc9b66b8387b
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2024-06-01 16:39:13 -0300

    [redirector] Make redirects dynamic

--- redirector/main.go
+++ redirector/main.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 const STATUS_TEMPORARY_REDIRECTION = 302
 
+func GetTarget(path string) string {
+	path = strings.ToLower(path[1:])
+	return "https://" + "www.google.com/search?q=" + url.QueryEscape(path)
+}
+
 func Redirect(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Response now!")
-	w.Header().Set("Location", "https://google.com/")
+	fmt.Println(r.URL.Path[1:])
+	w.Header().Set("Location", GetTarget(r.URL.Path))
 	w.WriteHeader(STATUS_TEMPORARY_REDIRECTION)
 }
 
