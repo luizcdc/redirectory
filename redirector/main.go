8d5e9c8d20d917c86c7ef73c1941183cf1a06cbf
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2024-06-04 22:29:44 -0300

    [redirector] Initialize cache on main

--- redirector/main.go
+++ redirector/main.go
@@ -15,8 +15,6 @@ const BAD_REQUEST = 400
 const NOT_FOUND = 404
 const SECONDS = 1e9
 
-
-
 func GetTarget(path string) string {
 	path = strings.ToLower(path)
 	return "https://" + "www.google.com/search?q=" + url.QueryEscape(path)
@@ -31,28 +29,28 @@ func SetRedirect(w http.ResponseWriter, r *http.Request) {
 	params := strings.Split(r.URL.Path[len("/SetRedirect/"):], "/")[:2]
 	key, value := params[0], params[1]
 	fmt.Println(key, value)
-	if records.SetKey(key, value, 10 * SECONDS) {
+	if records.SetKey(key, value, 10*SECONDS) {
 		fmt.Printf("Success setting '%v' to '%v'\n", key, value)
 		return
 	}
 	fmt.Println("FAILED!")
 }
 
-
-func Redirect (w http.ResponseWriter, r * http.Request) {
-		key := r.URL.Path[1:]
-		redirectTo, err := records.GetString(key)
-		if err != nil {
-			log.Printf("Error (%v): no redirect for key '%v'\n", err.Error(), key)
-			w.WriteHeader(NOT_FOUND)
-			w.Write([]byte(fmt.Sprintf("<h1>Error %v: URL not found!</h1>", NOT_FOUND)))
-			return
-		}
-		w.Header().Set("Location", GetTarget(redirectTo))
-		w.WriteHeader(TEMPORARY_REDIRECTION)
+func Redirect(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.Path[1:]
+	redirectTo, err := records.GetString(key)
+	if err != nil {
+		log.Printf("Error (%v): no redirect for key '%v'\n", err.Error(), key)
+		w.WriteHeader(NOT_FOUND)
+		w.Write([]byte(fmt.Sprintf("<h1>Error %v: URL not found!</h1>", NOT_FOUND)))
+		return
+	}
+	w.Header().Set("Location", GetTarget(redirectTo))
+	w.WriteHeader(TEMPORARY_REDIRECTION)
 }
 
 func main() {
+	records.MakeCache(5)
 	http.HandleFunc("/setredirect", SetRedirect)
 	http.HandleFunc("/setredirect/", SetRedirect)
 	http.HandleFunc("/", Redirect)
