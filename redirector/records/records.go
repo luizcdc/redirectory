a33a19105e5cf00ec944c6ec024c554e4645b87a
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2024-06-04 22:25:09 -0300

    [redirector] Use cache when setting or retrieving keys on redis

--- redirector/records/records.go
+++ redirector/records/records.go
@@ -9,8 +9,20 @@ import (
 	"context"
 	"time"
 
+	"github.com/luizcdc/redirectory/redirector/records/lru_cache"
 	"github.com/luizcdc/redirectory/redirector/records/redis_client"
 )
+
+var cache *lru_cache.LRUCache
+
+func MakeCache(cap uint) {
+	// TODO: set up environment variable for cache duration
+	if cache == nil {
+		cache = lru_cache.NewCache(cap, 6e11)
+	} else {
+		cache.ChangeCap(cap)
+	}
+}
 
 // SetKey returns a bool indicating success of the specified set operation.
 func SetKey(key string, value interface{}, ttl time.Duration) bool {
@@ -18,11 +30,22 @@ func SetKey(key string, value interface{}, ttl time.Duration) bool {
 	if err != nil {
 		return false
 	}
-	return client.Set(context.Background(), key, value, ttl).Err() == nil
+	success := client.Set(context.Background(), key, value, ttl).Err() == nil
+	if success {
+		cache.Insert(key, value)
+	}
+	return success
 }
 
 // GetString retrieves a string value from Redis.
-func GetString(key string)  (string, error) {
+func GetString(key string) (string, error) {
+	value, ok := cache.Fetch(key)
+	if ok {
+		return_value, ok := value.(string)
+		if ok {
+			return return_value, nil
+		}
+	}
 	client, err := redis_client.GetClientInstance()
 	if err != nil {
 		return "", err
