9bbe1f4cf43f55d3654b9174fdc00899af075b73
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2024-06-09 15:31:44 -0300

    [redirector] Refactor SetRedirect to simplify and reduce code duplication

--- redirector/main.go
+++ redirector/main.go
@@ -18,7 +18,8 @@ import (
 	"github.com/luizcdc/redirectory/redirector/records"
 )
 
-// loadEnv loads environment variables from a .env file if the application is not running on Google App Engine.
+// loadEnv loads environment variables from a .env file if the application is not running on
+// Google App Engine.
 func loadEnv() {
 	if os.Getenv("GAE_APPLICATION") != "" {
 		log.Println("Running on Google App Engine, environment variables are already set.")
@@ -30,87 +31,106 @@ func loadEnv() {
 	}
 }
 
+// simpleErrorJSONReply is a higher-order function that returns a function
+// responsible for sending a JSON response with the specified status code
+// and error message in the "error" field.
+//
+// Parameters:
+//   - w: The http.ResponseWriter that will write the response.
+//
+// Returns:
+//
+//	A function (status int, err interface{}) that and sends a JSON response with the specified
+//
+// status code and error message in the "error" field.
+//
+// Example usage:
+//
+//	errorHandler := simpleErrorJSONReply(w)
+//	errorHandler(http.StatusInternalServerError, "Internal Server Error because...")
+func simpleErrorJSONReply(w http.ResponseWriter) func(int, interface{}) {
+	return func(status int, err interface{}) {
+		w.WriteHeader(status)
+		resp, _ := json.Marshal(struct {
+			Error interface{} `json:"error"`
+		}{err})
+		w.Write(resp)
+	}
+}
+
 // SetRedirect sets a redirect for a given path.
 // It expects a JSON payload in the request body with the following structure:
-// {
-//   "url": "https://example.com",
-//   "duration": 10
-// }
-// The "url" field specifies the target URL for the redirect, and the "duration" field (optional) 
+//
+//	{
+//	  "url": "https://example.com",
+//	  "duration": 10
+//	}
+//
+// The "url" field specifies the target URL for the redirect, and the "duration" field (optional)
 // specifies the duration of the redirect in seconds.
 // The function returns a JSON response indicating the success or failure of setting the redirect.
 // If the redirect is set successfully, the response will be:
-// {
-//   "error": null
-// }
+//
+//	{
+//	  "error": null
+//	}
+//
 // If there is an error in setting the redirect, the response will be:
-// {
-//   "error": "failure message"
-// }
+//
+//	{
+//	  "error": "failure message"
+//	}
 func SetRedirect(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	type setRedirectBody struct {
 		Url      string `json:"url"`
 		Duration uint   `json:"duration"`
 	}
+
+	reply := simpleErrorJSONReply(w)
+
 	w.Header().Add("Content-Type", "application/json")
 	switch {
 	case len(ps.ByName("path")) < 4:
-		w.WriteHeader(http.StatusBadRequest)
-		resp, _ := json.Marshal(map[string]string{"error": "path must be at least 4 characters long"})
-		w.Write(resp)
+		reply(http.StatusBadRequest, "path must be at least 4 characters long")
 		return
 	case !strings.Contains(r.Header.Get("content-type"), "application/json"):
-		w.WriteHeader(http.StatusBadRequest)
-		resp, _ := json.Marshal(map[string]string{"error": "Content-Type must be 'application/json'"})
-		w.Write(resp)
+		reply(http.StatusBadRequest, "Content-Type must be 'application/json'")
 		return
 	}
 
 	length, err := strconv.Atoi(r.Header.Get("content-length"))
 	if err != nil {
-		fmt.Println(err.Error())
-		w.WriteHeader(http.StatusBadRequest)
-		resp, _ := json.Marshal(map[string]string{"error": "Content-Length header is required"})
-		w.Write(resp)
+		log.Println(err.Error())
+		reply(http.StatusBadRequest, "Content-Length header is required")
 		return
 	}
 	from := ps.ByName("path")
 
-	buffer := make([]byte, max(length, int(math.Pow(2, 16))))
+	buffer := make([]byte, min(length, int(math.Pow(2, 16))))
 	n, err := r.Body.Read(buffer)
 	if err != nil && err != io.EOF {
-		fmt.Println(err.Error())
-		w.WriteHeader(500)
-		resp, _ := json.Marshal(map[string]string{"error": fmt.Sprintf("error reading the request's body: %v", err.Error())})
-		w.Write(resp)
+		log.Println(err.Error())
+		reply(http.StatusInternalServerError, fmt.Sprintf("error reading the request's body: %v", err.Error()))
 		return
 	}
 	var jsonBody setRedirectBody
 	err = json.Unmarshal(buffer[:n], &jsonBody)
 	if err != nil {
-		fmt.Println(err)
-		w.WriteHeader(http.StatusBadRequest)
-		resp, _ := json.Marshal(map[string]string{"error": fmt.Sprintf("error parsing json in the request's body: %v", err.Error())})
-		w.Write(resp)
+		log.Println(err)
+		reply(http.StatusBadRequest, fmt.Sprintf("error parsing json in the request's body: %v", err.Error()))
 		return
 	}
-	fmt.Println(from, jsonBody.Url, jsonBody.Duration)
+	log.Println(from, jsonBody.Url, jsonBody.Duration)
 	parsedUrl, err := url.Parse(jsonBody.Url)
 	switch {
 	case err != nil:
-		fmt.Println(err)
-		w.WriteHeader(http.StatusBadRequest)
-		resp, _ := json.Marshal(map[string]string{"error": fmt.Sprintf("the provided url is invalid: %v", err.Error())})
-		w.Write(resp)
+		log.Println(err)
+		reply(http.StatusBadRequest, fmt.Sprintf("the provided url is invalid: %v", err.Error()))
 		return
 	case !parsedUrl.IsAbs():
-		fmt.Println(err)
-		w.WriteHeader(http.StatusBadRequest)
-		resp, _ := json.Marshal(map[string]string{"error": fmt.Sprintf("the provided url must be absolute")})
-		w.Write(resp)
+		log.Println(err)
+		reply(http.StatusBadRequest, "the provided url must be absolute")
 		return
-	case parsedUrl.Scheme == "":
-		parsedUrl.Scheme = "https"
 	}
 
 	duration := 10
@@ -119,22 +139,19 @@ func SetRedirect(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	}
 
 	if records.SetKey(from, parsedUrl.String(), time.Duration(duration)*time.Second) {
-		fmt.Printf("Success setting '%v' to '%v'\n", from, parsedUrl.String())
-		resp, _ := json.Marshal(map[string]interface{}{"error": nil})
-		w.Write(resp)
+		log.Printf("Success setting '%v' to '%v'\n", from, parsedUrl.String())
+		reply(http.StatusOK, nil)
 		return
 	}
 
-	resp, _ := json.Marshal(map[string]string{"error": fmt.Sprintf("failure setting '%v' to '%v'", from, parsedUrl.String())})
-	w.Write(resp)
-	fmt.Printf("Failure setting '%v' to '%v'\n", from, parsedUrl.String())
+	reply(http.StatusInternalServerError, fmt.Sprintf("failure setting '%v' to '%v'", from, parsedUrl.String()))
+	log.Printf("Failure setting '%v' to '%v'\n", from, parsedUrl.String())
 
 }
 
 // Redirect serves the redirect request for a previously set redirect path.
 func Redirect(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	key := ps.ByName("redirectpath")[1:]
-	fmt.Println(key)
 	redirecto, err := records.GetString(key)
 	if err != nil {
 		log.Printf("Error: no redirect for key '%v'\n", key)
