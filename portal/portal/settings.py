15a35b59e61128d00f9ec46c63abf0988f263ad5
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2024-06-24 16:18:43 -0300

    [portal] Separate dev and prod environments

--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('ENV').lower() == 'dev'
 
 ALLOWED_HOSTS = []
 
